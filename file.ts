ce00c4239537c26054180fcd6de81870474342aa
Marani Matias Ezequiel <maranimatias@gmail.com>
2016-07-08 18:25:14 -0300

    file.ts and new code

---
+++ file.ts
@@ -0,0 +1,56 @@
+type ColorObject = {r: number, g: number, b: number, a?: number};
+type Axes = { x:number, y:number, z?:number};
+interface Line {
+  comment  :string;
+  colour   :ColorObject;
+  axes     :Axes;
+  show     :boolean;
+  code()   :string;
+}
+import * as fs from "fs";
+export default
+ /**
+ * File
+ */
+class File {
+  
+  /**
+   * Creates an instance of File.
+   * 
+   * @param {string[]} [gCodeInit]
+   */
+  constructor( gCodeInit ?:string[] ) {
+    if(gCodeInit){
+      this._gCodeInit = gCodeInit;
+    }else{
+      this._gCodeInit = [
+        'G21 ; Set units to mm',
+        'G90 ; Absolute positioning',
+        'G01 X0 Y0 Z765; con Z max',
+      ]
+    }
+  }
+  
+  private _gCodeInit : string[];
+  private Concat(gCode: Line[]) :string[] {
+    for (let index = 0; index < gCode.length; index++) {
+      let element = gCode[index];
+      if(element.show){ this._gCodeInit.push( element.code() ); }
+    }
+    return this._gCodeInit;
+  }
+  
+  /**
+   * Save 
+   * 
+   * @param {string} dirGCode path the gCode file
+   * @param {Line[]} gCode array de lineas para converti en gcode
+   */
+  public Save(dirGCode :string, gCode: Line[] , cb?: () => void ) {
+    fs.unlink(dirGCode);
+    fs.writeFile(dirGCode, this.Concat(gCode).join('\n'),{ encoding: "utf8" },(err)=>{
+      if(err) throw err.message;
+      if(cb) cb();
+    });
+  }
+}
--- line.ts
+++ line.ts
@@ -1,66 +1,46 @@
-interface pixel {r:number; g:number; b:number; a:number;}
-interface axes { x:number; y:number; z?:number;}
+/// <reference path="./typings/globals/lwip/index.d.ts" />
+type ColorObject = {r: number, g: number, b: number, a?: number};
+type Axes = { x:number, y:number, z?:number};
 /**
- * line
+ * Line
  */
-export default class line {
-
-  /*constructor( x: any ,y?:number,z?:number, comment? : string ) {
-    if ( y ===undefined && z === undefined ) {
-      this._code=x;
-    } 
-    if( x != undefined && y != undefined && z != undefined ) {
-      this._x=x; this._y=y; this._z=z;
-      this._code = `G01 X${this._x} Y${this._y} Z${this._z}`+ (comment ? ' ; ' + comment :'');
-    }
-    if(  x == Array(2) ){
-      this._x = x[0];
-      this._y = x[1];
-      this._code = `G01 X${this._x} Y${this._y}`+ (comment ? ' ; ' + comment :'');
-    }
-  }*/
-
-  
+export default class Line { 
   /**
    * @param  {x:number; y:number; z?:number;} axes 
    * @param  {r:number; g:number; b:number; a:number;} colour?
    * @param  {string} comment? 
    */
-  constructor(axes:any,colour?:pixel,comment?:string){
-    this._axes=axes;
+  constructor(show:boolean,axes:any,colour?:ColorObject,comment?:string){
+    this._axes = axes;
+    this._show = show;
     if(this._colour){ this._colour=colour; }
     if(this._colour){ this._comment=comment; }
   }
 
-  private _axes : axes;
-  public get axes() : axes {
+  private _axes : Axes;
+  public get axes() : Axes {
     return this._axes;
   }
-  public set axes(v : axes) {
+  public set axes(v : Axes) {
     this._axes = v;
   }
 
-  private _colour : pixel;
-  public get colour() : pixel {
+  private _colour : ColorObject;
+  public get colour() : ColorObject {
     return this._colour;
   }
-  public set colour(v : pixel) {
+  public set colour(v : ColorObject) {
     this._colour = v;
   }
   
-  /**
-   * code
-   * Arma code for that line with data
-   */
-  public code() : string {
-    // si es solo un comentario G01 no estaria 
-    let x = this._axes.x!==undefined ? ` X${this._axes.x}`:'';
-    let y = this._axes.y!==undefined ? ` Y${this._axes.y}`:'';
-    let z = this._axes.z!==undefined ? ` Z${this._axes.z}`:'';
-    let comment = this._comment ? `; ${this._comment}`:'';
-    return "G01"+x+y+z+comment;
+  private _show : boolean;
+  public get show() : boolean {
+    return this._show;
   }
-  
+  public set show(v : boolean) {
+    this._show = v;
+  }
+    
   private _comment : string;
   public get comment() : string {
     return this._comment;
@@ -69,5 +49,17 @@ export default class line {
     this._comment = v;
   }
   
+  /**
+   * code
+   * Arma code for that line with data
+   */
+  public code() : string {
+    // si es solo un comentario G01 no estaria
+    let s = this._show ? '':';';
+    let x = this._axes.x!==undefined ? ` X${this._axes.x}`:'';
+    let y = this._axes.y!==undefined ? ` Y${this._axes.y}`:'';
+    let z = this._axes.z!==undefined ? ` Z${this._axes.z}`:'';
+    let comment = this._comment ? `; ${this._comment}`:'';
+    return s+"G01"+x+y+z+comment;
+  }
 }
-
